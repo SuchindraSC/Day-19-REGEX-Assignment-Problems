af457f5e652f52fcaad334a79394e6a684c1e8cb
SuchindraSC <85979972+SuchindraSC@users.noreply.github.com>
2021-08-28 20:04:13 +0530

    Update Program.cs

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,43 +7,43 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             //Use Case 1
-            //Console.WriteLine("Use Case 1");
-            //Console.WriteLine("\n");
-            //Console.WriteLine("Enter The First Name: ");
-            //string userFirstName = Console.ReadLine();
-            //FirstName.validateFirstName(userFirstName);
+            Console.WriteLine("Use Case 1");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The First Name: ");
+            string userFirstName = Console.ReadLine();
+            FirstName.validateFirstName(userFirstName);
 
             //Use Case 2
-            //Console.WriteLine("Use Case 2");
-            //Console.WriteLine("\n");
-            //Console.WriteLine("Enter The Last Name: ");
-            //string userLastName = Console.ReadLine();
-            //LastName.validateLastName(userLastName);
+            Console.WriteLine("Use Case 2");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The Last Name: ");
+            string userLastName = Console.ReadLine();
+            LastName.validateLastName(userLastName);
 
             //Use Case 3
-            //Console.WriteLine("Use Case 3");
-            //Console.WriteLine("\n");
-            //Console.WriteLine("Enter The Email Id: ");
-            //string userEmailId = Console.ReadLine();
-            //EmailId.validateEmailId(userEmailId);
+            Console.WriteLine("Use Case 3");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The Email Id: ");
+            string userEmailId = Console.ReadLine();
+            EmailId.validateEmailId(userEmailId);
 
             //Use Case 4
-            //Console.WriteLine("Use Case 4");
-            //Console.WriteLine("\n");
-            //Console.WriteLine("Enter The Phone Number: ");
-            //string userPhoneNumber = Console.ReadLine();
-            //PhoneNumber.validatePhoneNumber(userPhoneNumber);
+            Console.WriteLine("Use Case 4");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter The Phone Number: ");
+            string userPhoneNumber = Console.ReadLine();
+            PhoneNumber.validatePhoneNumber(userPhoneNumber);
 
             //Use Case 5
-            //Console.WriteLine("Use Case 5");
-            //Console.WriteLine("\n");
-            //Console.WriteLine("RULE 1 --> Must Have Minimum 8 Charachters");
-            //Console.WriteLine("RULE 2 --> Must Have Atleast 1 Uppercase Charachter");
-            //Console.WriteLine("RULE 3 --> Must Have Atleat 1 Number");
-            //Console.WriteLine("RULE 4 --> Must Have Atleast 1 Special Character");
-            //Console.WriteLine("\nEnter Your Password: ");
-            //string userPassword = Console.ReadLine();
-            //Password.validatePassword1(userPassword);
+            Console.WriteLine("Use Case 5");
+            Console.WriteLine("\n");
+            Console.WriteLine("RULE 1 --> Must Have Minimum 8 Charachters");
+            Console.WriteLine("RULE 2 --> Must Have Atleast 1 Uppercase Charachter");
+            Console.WriteLine("RULE 3 --> Must Have Atleat 1 Number");
+            Console.WriteLine("RULE 4 --> Must Have Atleast 1 Special Character");
+            Console.WriteLine("\nEnter Your Password: ");
+            string userPassword = Console.ReadLine();
+            Password.validatePassword1(userPassword);
 
             //Sample Email IDs
             Console.WriteLine("Sample Email ID's");
